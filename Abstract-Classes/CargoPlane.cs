bd25feb1a5d60a72168adafa00e192a0f76338fc
imp111 <alexsandartenev@gmail.com>
2021-05-07 12:52:38 +0300

    second commit

---
+++ Abstract-Classes/CargoPlane.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    public class CargoPlane : JetPlane
+    {
+        public override string Type
+        {
+            get
+            {
+                return "CargoPlane";
+            }
+        }
+
+        public override int Speed
+        {
+            get
+            {
+                return 700;
+            }
+        }
+
+        public override void RunAutopilot()
+        {
+            Console.WriteLine($"The {this.Type} is in autopilot!");
+        }
+    }
+}
---
+++ Abstract-Classes/Plance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    public abstract class Plane : IComparable<Plane>
+    {
+        public abstract string Type { get; }
+        public abstract int Speed { get; }
+
+        public int CompareTo(Plane other)
+        {
+            return this.Speed.CompareTo(other.Speed);
+        }
+
+        public override string ToString()
+        {
+            string theString = "This is: " + this.Type;
+            return theString;
+        }
+    }
+}
